a9fa79e5b535383a20d5edcad8e69482f968ad05
Kosenkov <i.kosenkov77@yandex.ru>
2024-09-11 19:59:16 +0300

    init

---
+++ src/main/java/pro/kosenkov/open_api_generator/OpenApiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pro.kosenkov.open_api_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/kosenkov/open_api_generator/OpenApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.kosenkov.open_api_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
